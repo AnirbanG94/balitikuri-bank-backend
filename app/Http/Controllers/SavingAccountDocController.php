009800484358251f93c010242a5ba2e4b1b872a2
Anirban Ghosh <shovon.electrical59459@gmail.com>
2026-06-16 19:13:28 +0530

    update saving account document controller

--- app/Http/Controllers/SavingAccountDocController.php
+++ app/Http/Controllers/SavingAccountDocController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\DocReqSavingAccount;
 use App\Models\ValidAndAcceptableDocForSavingsAcc;
+// **** newly added feature *****
 
 class SavingAccountDocController extends Controller
 {
